d7a73fac210ec10e97c5324a217bef287b19c342
pradeepGZ <pradeep@email.gozoop.com>
2026-05-15 18:19:41 +0530

    changes by pradeep - 01

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
-# Handle by pradeep
+# Handle by pradeep new 1
 
 import time
 import instaloader
